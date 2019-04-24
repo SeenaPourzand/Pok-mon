339d49c4a17d83abd6eeb595d14dcb082869bf86
phoebetang <phoebetang@10.141.93.98>
2019-04-24 12:34:16 -0700

    commited

---
+++ PokemonAPCS/src/PokemonGame.java
@@ -0,0 +1,8 @@
+
+public class PokemonGame {
+	
+	public PokemonGame() {
+
+	}
+	
+}
---
+++ PokemonAPCS/src/pokemon.java
@@ -0,0 +1,12 @@
+
+public class pokemon {
+	private final String type;
+
+	public pokemon(String type) {
+		this.type = type;
+	}
+
+	String getType() {
+		return type;
+	}
+}
