d5334e3dce17805c24e42a24f76c6ffa549154ad
gwanway <gwanway@gmail.com>
2019-04-25 12:34:34 -0700

    test

--- PokemonAPCS/src/pokemon.java
+++ PokemonAPCS/src/pokemon.java
@@ -6,25 +6,21 @@ public class pokemon { // all parameters requiring a string
 	private int stage;
 	private int hp;
 	private int attack;
-	private int spk.attack;
+
 	private int defense;
-	private int spk.defesne;
+
 	private int speed;
 	private int exp_Needed;
-	
-	private final String type;
+
 
 	public pokemon(String type) {
 	
 	}
 
-<<<<<<< HEAD
-	
-=======
+
 	String getType() {
 		
 		return type;
 	}
-String test;
->>>>>>> branch 'master' of https://github.com/SeenaPourzand/Pok-mon.git
+
 }
